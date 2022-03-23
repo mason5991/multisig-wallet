f30f62750680f3b63758c7a32ca3728586e61e3f
Mason Mung <mason5991@yahoo.com.hk>
2022-03-24 01:07:19 +0800

    update contract

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,7 +1,6 @@
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
-import detectEthereumProvider from '@metamask/detect-provider';
-import Wallet from 'backend/build/contracts/Wallet.json';
+import Wallet from '../contracts/Wallet.json';
 
 export const getInstance = async (web3: Web3) => {
   const networkId = await web3.eth.net.getId();
