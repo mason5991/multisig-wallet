d9647bf791b44440bec50284f71f4e28337108c2
Mason Mung <mason5991@yahoo.com.hk>
2022-03-24 14:39:31 +0800

    fix contract address missing

--- client/src/containers/Home.tsx
+++ client/src/containers/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Box, VStack, Grid, theme, Spinner, Container } from '@chakra-ui/react';
+import { Box, VStack, Grid, theme, Spinner, Container, Text } from '@chakra-ui/react';
 import Header from '../components/Header';
 import TransferForm, { OnSubmitFunc as CreateTransferFunc } from '../components/TransferForm';
 import TransferList from '../components/TransferList';
@@ -11,6 +11,7 @@ import { getInstance } from '../utils';
 const Home = () => {
   const { web3, account } = useWeb3Context();
   const [wallet, setWallet] = useState<Wallet | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(false);
   const [accounts, setAccounts] = useState<string[] | undefined>(undefined);
   const [approvers, setApprovers] = useState<string[]>([]);
   const [quorum, setQuorum] = useState<string | undefined>(undefined);
@@ -27,9 +28,14 @@ const Home = () => {
   useEffect(() => {
     const init = async () => {
       if (web3) {
+        setIsLoading(true);
         const instance = await getInstance(web3);
         console.log('instance===', instance);
-        setWallet(new Wallet(instance as unknown as WalletContract));
+        if (instance) {
+          setWallet(new Wallet(instance as unknown as WalletContract));
+        }
+
+        setIsLoading(false);
       }
     };
     init();
@@ -38,6 +44,7 @@ const Home = () => {
   useEffect(() => {
     const init = async () => {
       if (wallet) {
+        setIsLoading(true);
         const chainId = await web3!.eth.getChainId();
         console.log(chainId);
         const accounts = await web3!.eth.getAccounts();
@@ -49,6 +56,7 @@ const Home = () => {
         setQuorum(quorum);
         setApprovers(approvers);
         setTransfers(transfers);
+        setIsLoading(false);
       }
     };
     init();
@@ -56,17 +64,25 @@ const Home = () => {
 
   return (
     <VStack spacing={8}>
-      {!wallet ? (
+      {isLoading ? (
         <Spinner />
       ) : (
         <>
-          {approvers.length > 0 && quorum && <Header approvers={approvers} quorum={quorum} />}
-          <Container>
-            <TransferForm onSubmit={createTransfer} />
-          </Container>
+          {!wallet ? (
+            <Text>Deployed address not found</Text>
+          ) : (
+            <>
+              {approvers.length > 0 && quorum && <Header approvers={approvers} quorum={quorum} />}
+              <Container>
+                <TransferForm onSubmit={createTransfer} />
+              </Container>
+              <Container>
+                <TransferList transfers={transfers} approveTransfer={approveTransfer} />
+              </Container>
+            </>
+          )}
         </>
       )}
-      <TransferList transfers={transfers} approveTransfer={approveTransfer} />
     </VStack>
   );
 };
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,11 +1,24 @@
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
-import Wallet from '../contracts/Wallet.json';
+import config from '../configs/prod.json';
+
+interface Config {
+  networks: {
+    [key: string]: {
+      address: string;
+    };
+  };
+  abi: AbiItem[];
+}
 
 export const getInstance = async (web3: Web3) => {
   const networkId = await web3.eth.net.getId();
   console.log('networkId===', networkId);
-  const deployedNetwork = (Wallet.networks as any)[networkId];
+  const { networks, abi } = config as unknown as Config;
+  const deployedNetwork = networks[networkId];
+  if (!deployedNetwork) {
+    return false;
+  }
   console.log('deployedNetwork====', deployedNetwork);
-  return new web3.eth.Contract(Wallet.abi as AbiItem[], deployedNetwork?.address);
+  return new web3.eth.Contract(abi, deployedNetwork?.address);
 };
